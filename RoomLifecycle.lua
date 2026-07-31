78273863a0975c69074f33359a7b771cd5982b43
GabiR1221 <gabrielrusu0411@gmail.com>
2026-07-31 23:32:54 +0300

    Update RoomLifecycle.lua

--- RoomLifecycle.lua
+++ RoomLifecycle.lua
@@ -1,3 +1,4 @@
+--Modulescript in Services folder in ServerScriptService
 --!strict
 local ReplicatedStorage=game:GetService("ReplicatedStorage")
 local Constants=require(ReplicatedStorage.Shared.RoomConstants)
