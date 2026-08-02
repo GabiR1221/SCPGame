4f6e97cb1d4976ecb22519a794b33f836fecad60
GabiR1221 <gabrielrusu0411@gmail.com>
2026-08-02 05:34:06 +0300

    Update CharacterConfig.lua

--- CharacterConfig.lua
+++ CharacterConfig.lua
@@ -5,7 +5,7 @@ return table.freeze({
 	CrouchHipHeightOffset = -1.15, CrouchToggleCooldown = 0.15,
 	TestingAllowFirstPersonToggle = true,
 	Camera = table.freeze({DefaultFov=70, FirstPersonForwardOffset=.62, DownwardForwardBonus=.58, FirstPersonVerticalOffset=.14, HeadBobEnabled=true, ShakeEnabled=true, HeadBobAmount=0.035, HeadBobFrequency=9}),
-	FirstPersonBody = table.freeze({ForwardOffset=.28,DownwardForwardBonus=.24,ForwardSign=-1,AlignCharacterYaw=true}),
+	FirstPersonBody = table.freeze({ForwardOffset=.28,DownwardForwardBonus=.24,ForwardSign=-1,AlignCharacterYaw=true,HorizontalDirectionEpsilon=.001,DebugInterval=.5}),
 	LookBend = table.freeze({Enabled=true, MaximumPitch=math.rad(55), WaistWeight=.65, NeckWeight=.35, SmoothSpeed=14}),
 	Debug = false,
 })
