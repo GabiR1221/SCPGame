3245f14ef87beae0e8b8b8f258f2ead4d1463c37
GabiR1221 <gabrielrusu0411@gmail.com>
2026-08-05 02:25:30 +0300

    Update InteractionConfig.lua

--- InteractionConfig.lua
+++ InteractionConfig.lua
@@ -6,7 +6,7 @@ return table.freeze({
 	Definitions=table.freeze({
 		OpenDrawer={AnimationKey="OpenDrawer",Duration=1.2,CommitTime=.3,Locks={"Movement","Jump","Interaction","Rotation"},NoiseRadius=22},
 		CloseDrawer={AnimationKey="CloseDrawer",Duration=1.2,CommitTime=.3,Locks={"Movement","Jump","Interaction","Rotation"},NoiseRadius=12},
-		Locker={AnimationKey="LockerEnter",Duration=.25,CommitTime=.05,Locks={"Interaction"},NoiseRadius=8,UseTargetCamera=false},
+		Locker={AnimationKey="LockerEnter",Duration=.7,CommitTime=.05,Locks={"Interaction"},NoiseRadius=8,UseTargetCamera=false},
 		PickupTool={AnimationKey=nil,Duration=.2,CommitTime=.05,Locks={"Interaction"},NoiseRadius=4},
 		PullLever={AnimationKey="PullLever",Duration=1.5,CommitTime=.75,Locks={"Movement","Jump","Interaction","Rotation"},NoiseRadius=28},
 		PressButton={AnimationKey=nil,Duration=.35,CommitTime=.1,Locks={"Interaction"},NoiseRadius=12},
