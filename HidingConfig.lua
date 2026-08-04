25c67bc406d750db3c3f41c7c3b88ba083e582ce
GabiR1221 <gabrielrusu0411@gmail.com>
2026-08-04 22:50:28 +0300

    Create HidingConfig.lua

---
+++ HidingConfig.lua
@@ -0,0 +1,3 @@
+--!strict
+-- ModuleScript: ReplicatedStorage/Shared/HidingConfig
+return table.freeze({RequestCooldown=.35,MaximumDistance=8,EnterDelay=.12,ExitDelay=.25,UseFixedCamera=false,CameraFov=70,Locks={"Movement","Jump","Rotation","Interaction","Tool","Inventory","Crouch"}})
