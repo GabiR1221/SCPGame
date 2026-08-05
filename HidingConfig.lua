c5e1b95f03a00e56494c448f578c62759b36d0e3
GabiR1221 <gabrielrusu0411@gmail.com>
2026-08-05 04:43:47 +0300

    Update HidingConfig.lua

--- HidingConfig.lua
+++ HidingConfig.lua
@@ -1,3 +1,3 @@
 --!strict
 -- ModuleScript: ReplicatedStorage/Shared/HidingConfig
-return table.freeze({RequestCooldown=.35,MaximumDistance=8,EnterDuration=.7,EnterCommitTime=.35,ExitDuration=.65,ExitCommitTime=.25,AnchorRootWhileHidden=true,UnequipToolsOnEnter=true,UseFixedCamera=false,CameraFov=70,Locks={"Movement","Jump","Rotation","Interaction","Tool","Inventory","Crouch"}})
+return table.freeze({RequestCooldown=.35,MaximumDistance=8,EnterDuration=.7,EnterCommitTime=.35,ExitDuration=.65,ExitCommitTime=.25,AnchorRootWhileHidden=true,UnequipToolsOnEnter=true,EnterRequestFallbackDuration=1,UseFixedCamera=false,CameraFov=70,Locks={"Movement","Jump","Rotation","Interaction","Tool","Inventory","Crouch"}})
