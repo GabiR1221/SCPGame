273e6ffcd11b4b3347f76dd78d8f8362ab0b772e
GabiR1221 <gabrielrusu0411@gmail.com>
2026-08-12 15:44:57 +0300

    Update AnimationConfig.lua

--- AnimationConfig.lua
+++ AnimationConfig.lua
@@ -3,6 +3,7 @@
 -- Replace 0 with animations owned by the experience owner/group before testing.
 return table.freeze({
 	FadeIn=.18, FadeOut=.16, MinSpeed=.55, MaxSpeed=1.65,
+	InteractionTimingDebug=false, HidingDebug=false, StabilizeRootTranslationWhileHidden=true, RootTranslationTolerance=.05,
 	DirectionalMovement=table.freeze({Enabled=true,DirectionSwitchThreshold=.12,DirectionHysteresis=.15,MinimumDirectionalSpeed=.5}),
 	Animations=table.freeze({
 		Idle={Id="rbxassetid://507766666",Priority=Enum.AnimationPriority.Idle,Looped=true,AuthoredSpeed=1},
@@ -22,7 +23,7 @@ return table.freeze({
 		OpenDrawer={Id="rbxassetid://109284362906915",Priority=Enum.AnimationPriority.Action2,Looped=false,AuthoredSpeed=1},
 		CloseDrawer={Id="rbxassetid://109284362906915",Priority=Enum.AnimationPriority.Action2,Looped=false,AuthoredSpeed=1},
 		LockerEnter={Id="rbxassetid://72838676764108",Priority=Enum.AnimationPriority.Action3,Looped=false,AuthoredSpeed=1},
-		LockerIdle={Id="rbxassetid://101403921204911",Priority=Enum.AnimationPriority.Action,Looped=true,AuthoredSpeed=1},
+		LockerIdle={Id="rbxassetid://101403921204911",Priority=Enum.AnimationPriority.Action3,Looped=true,AuthoredSpeed=1},
 		LockerExit={Id="rbxassetid://113984487264215",Priority=Enum.AnimationPriority.Action3,Looped=false,AuthoredSpeed=1},
 		PullLever={Id="rbxassetid://109284362906915",Priority=Enum.AnimationPriority.Action2,Looped=false,AuthoredSpeed=1},
 		IntroLook={Id="rbxassetid://109284362906915",Priority=Enum.AnimationPriority.Action3,Looped=false,AuthoredSpeed=1},
@@ -32,17 +33,17 @@ return table.freeze({
 		FlashlightUnequip={Id="rbxassetid://118308237201245",Priority=Enum.AnimationPriority.Action2,Looped=false,AuthoredSpeed=1},
 		FlashlightToggle={Id="rbxassetid://129785789637011",Priority=Enum.AnimationPriority.Action2,Looped=false,AuthoredSpeed=1},
 		-- These play only on ReplicatedStorage.ClientAssets.Viewmodels.Flashlight.
-		FlashlightViewmodelEquip={Id="rbxassetid://110714371172299",Priority=Enum.AnimationPriority.Action,Looped=false,AuthoredSpeed=1},
-		FlashlightViewmodelIdle={Id="rbxassetid://90500203310017",Priority=Enum.AnimationPriority.Idle,Looped=true,AuthoredSpeed=1},
-		FlashlightViewmodelToggle={Id="rbxassetid://129785789637011",Priority=Enum.AnimationPriority.Action2,Looped=false,AuthoredSpeed=1},
-		FlashlightViewmodelUnequip={Id="rbxassetid://118308237201245",Priority=Enum.AnimationPriority.Action,Looped=false,AuthoredSpeed=1},
+		FlashlightViewmodelEquip={Id="rbxassetid://0",Priority=Enum.AnimationPriority.Action,Looped=false,AuthoredSpeed=1},
+		FlashlightViewmodelIdle={Id="rbxassetid://0",Priority=Enum.AnimationPriority.Idle,Looped=true,AuthoredSpeed=1},
+		FlashlightViewmodelToggle={Id="rbxassetid://0",Priority=Enum.AnimationPriority.Action2,Looped=false,AuthoredSpeed=1},
+		FlashlightViewmodelUnequip={Id="rbxassetid://0",Priority=Enum.AnimationPriority.Action,Looped=false,AuthoredSpeed=1},
 
 	}),
 	ObjectAnimationDebug=false,
 	ObjectAnimations=table.freeze({
-		DrawerOpen={Id="rbxassetid://88542616058118",Priority=Enum.AnimationPriority.Action,Looped=false,Duration=.55,LengthTimeout=.25,FinalFrameEpsilon=.001,HoldLeadTime=.05},
-		DrawerClose={Id="rbxassetid://115769042595557",Priority=Enum.AnimationPriority.Action,Looped=false,Duration=.55,LengthTimeout=.25,FinalFrameEpsilon=.001,HoldLeadTime=.05},
-		LockerEnterObject={Id="rbxassetid://99308891064081",Priority=Enum.AnimationPriority.Action,Looped=false,Duration=.7,LengthTimeout=.25,FinalFrameEpsilon=.001,HoldLeadTime=.05},
-		LockerExitObject={Id="rbxassetid://85426657586918",Priority=Enum.AnimationPriority.Action,Looped=false,Duration=.65,LengthTimeout=.25,FinalFrameEpsilon=.001,HoldLeadTime=.05},
+		DrawerOpen={Id="rbxassetid://98982428590563",Priority=Enum.AnimationPriority.Action,Looped=false,Duration=.55,LengthTimeout=.25,FinalFrameEpsilon=.001,HoldLeadTime=.05},
+		DrawerClose={Id="rbxassetid://136649992761867",Priority=Enum.AnimationPriority.Action,Looped=false,Duration=.55,LengthTimeout=.25,FinalFrameEpsilon=.001,HoldLeadTime=.05},
+		LockerEnterObject={Id="rbxassetid://84722822165057",Priority=Enum.AnimationPriority.Action,Looped=false,Duration=.7,LengthTimeout=.25,FinalFrameEpsilon=.001,HoldLeadTime=.05},
+		LockerExitObject={Id="rbxassetid://115991174612221",Priority=Enum.AnimationPriority.Action,Looped=false,Duration=.65,LengthTimeout=.25,FinalFrameEpsilon=.001,HoldLeadTime=.05},
 	})
 })
