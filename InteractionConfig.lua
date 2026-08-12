acd68e22ac911e4fa23fb4f3b4771491c72376dd
GabiR1221 <gabrielrusu0411@gmail.com>
2026-08-12 15:46:56 +0300

    Update InteractionConfig.lua

--- InteractionConfig.lua
+++ InteractionConfig.lua
@@ -2,10 +2,11 @@
 -- ModuleScript: ReplicatedStorage/Shared/InteractionConfig
 return table.freeze({
 	Debug=true,
+	TimingDebug=false,
 	ScanDistance=12, DefaultDistance=8, MaxServerDistance=16, RequestBurst=4, RequestRefillPerSecond=2,
 	Definitions=table.freeze({
-		OpenDrawer={AnimationKey="OpenDrawer",Duration=1.2,CommitTime=.3,Locks={"Movement","Jump","Interaction","Rotation"},NoiseRadius=22},
-		CloseDrawer={AnimationKey="CloseDrawer",Duration=1.2,CommitTime=.3,Locks={"Movement","Jump","Interaction","Rotation"},NoiseRadius=12},
+		OpenDrawer={AnimationKey="OpenDrawer",Duration=1.2,CommitTime=.05,Locks={"Movement","Jump","Interaction","Rotation"},NoiseRadius=22},
+		CloseDrawer={AnimationKey="CloseDrawer",Duration=1.2,CommitTime=.05,Locks={"Movement","Jump","Interaction","Rotation"},NoiseRadius=12},
 		Locker={AnimationKey="LockerEnter",Duration=.7,CommitTime=.05,Locks={"Interaction"},NoiseRadius=8,UseTargetCamera=false},
 		PickupTool={AnimationKey=nil,Duration=.2,CommitTime=.05,Locks={"Interaction"},NoiseRadius=4},
 		PullLever={AnimationKey="PullLever",Duration=1.5,CommitTime=.75,Locks={"Movement","Jump","Interaction","Rotation"},NoiseRadius=28},
