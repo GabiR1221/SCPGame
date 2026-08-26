185d9dc410944d43287fe12451ad4792f7feec24
GabiR1221 <gabrielrusu0411@gmail.com>
2026-08-26 17:39:08 +0300

    Update EntityConfig.lua

--- EntityConfig.lua
+++ EntityConfig.lua
@@ -12,7 +12,20 @@ return table.freeze({
 		PrintDecisions=true,
 	}),
 	Entities=table.freeze({
+		Hert=table.freeze({
+			Director="RoomCreature",
+			Enabled=true,ForceSpawn=true,Debug=true,TemplateName="Hert",SpawnChance=.25,MaxHealth=100,Damage=25,
+			DetectionRange=35,HitRadius=3,JumpDuration=.55,JumpHeight=6,
+			MinimumAttackCooldown=3,MaximumAttackCooldown=5,RequireLineOfSight=true,
+			IdleAnimation="HertIdle",AttackAnimation="HertAttack",DeathAnimation="HertDeath",
+			Animations=table.freeze({
+				HertIdle=table.freeze({Id="rbxassetid://0",Looped=true}),
+				HertAttack=table.freeze({Id="rbxassetid://0",Looped=false}),
+				HertDeath=table.freeze({Id="rbxassetid://0",Looped=false}),
+			}),
+		}),
 		HallwayRush=table.freeze({
+			Director="Hallway",
 			Enabled=true,
 			TemplateName="HallwayRush",
 			MinimumTriggerRoom=7,
