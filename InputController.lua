bbd21ed4774060f6c1945a04f0f530868ba48c6c
GabiR1221 <gabrielrusu0411@gmail.com>
2026-08-26 17:39:58 +0300

    Update InputController.lua

--- InputController.lua
+++ InputController.lua
@@ -17,9 +17,8 @@ end
 function Controller.Start(firstPerson:any)
 	ContextActionService:BindAction("ToggleCrouch",crouchAction,true,Enum.KeyCode.C,Enum.KeyCode.ButtonB)
 	ContextActionService:SetTitle("ToggleCrouch","Crouch")
-	ContextActionService:BindAction("ToggleFirstPersonTest",function(_name:string,state:Enum.UserInputState,_input:InputObject)
-		if state==Enum.UserInputState.Begin then firstPerson:ToggleForTesting(); return Enum.ContextActionResult.Sink end
-		return Enum.ContextActionResult.Pass
-	end,false,Enum.KeyCode.V)
+	-- V is reserved for the per-run upgrade tree. First-person testing remains
+	-- available on F8 and does not conflict with normal gameplay input.
+	ContextActionService:BindAction("ToggleFirstPersonTest",function(_name:string,state:Enum.UserInputState,_input:InputObject) if state==Enum.UserInputState.Begin then firstPerson:ToggleForTesting(); return Enum.ContextActionResult.Sink end; return Enum.ContextActionResult.Pass end,false,Enum.KeyCode.F8)
 end
 return Controller
